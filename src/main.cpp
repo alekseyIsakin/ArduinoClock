8d2ac589d864a5f6733f7224bc919931e57d57db
alekseyIsakin <karahunforever@gmail.com>
2020-11-01 21:23:30 +0800

    pre-final version

--- src/main.cpp
+++ src/main.cpp
@@ -29,9 +29,13 @@
 
 void ReadString(byte*str, uint32_t lenght);
 
+uint32_t CompileSingleString(byte*str, uint32_t c_pos, bool erase=false);
+
 Arduino_ST7789 tft = Arduino_ST7789(TFT_DC, TFT_RST);
 // SingleString *ss;
-PageBuilder*pb;
+
+byte*arr = (byte*)malloc(64*sizeof(byte));
+byte*arr_old = (byte*)malloc(64*sizeof(byte));
 
 void setup() {
   Serial.begin(9600);
@@ -43,36 +47,43 @@ void setup() {
   tft.setTextSize(7);
   tft.setTextColor(BLUE);
   tft.print("Ready");
-  delay(500);
 
-  tft.fillScreen(BLACK);
+  delay(500);
+  tft.clearScreen();
   delay(100);
 }
 
+int cnt = 0;
+
 void loop() {
-  byte arr[] = {0x01,
-                0x01,0x00,0x0,
-                0x02,0x00,0x1f,
-                0x03,0x03,
-                0x04,'c','b','s','\0',
-                0x01,
-                0x01,0x00,0x48,
-                0x02,0x00,0xf1,
-                0x03,0x07,
-                0x04,'2','2',':','1','1','\0'};
-  
-  ReadString(arr, 30);
-  Serial.println("Loop");
-  delay(5*1000);
-  // pb->ReadString(arr, 16);
-  // pb->ReadString(arr2, 14);
-  // if (Serial.available()){
-  //     ss->NewString(Serial.readString());
-  //     ss->Draw();
-  // };
+  if ((millis() / 1000) % 2 == 0){
+    tft.fillCircle(208, 208, 16, YELLOW);
+  }
+  if ((millis() / 1000) % 2 == 1){
+    tft.fillCircle(208, 208, 16, GREEN);
+  }
+
+  if (Serial.available()){
+
+    // tft.setTextSize(1);
+    // tft.setCursor(0,0);
+
+    String s = Serial.readString();
+    for (int i = 0; i<s.length(); i++){
+      arr[i] = ((byte)s[i]);
+      // tft.println(arr[i]);
+    }
+
+    CompileSingleString(arr_old, 1, 1);
+    ReadString(arr, s.length());
+
+    for(int i=0; i<64; i++){
+      arr_old[i] = arr[i];
+    }
+  }
 }
 
-uint32_t CompileSingleString(byte*str, uint32_t c_pos)
+uint32_t CompileSingleString(byte*str, uint32_t c_pos, bool erase=false)
 {
   byte options = 1;
   uint32_t cur_pos = c_pos;
@@ -80,7 +91,7 @@ uint32_t CompileSingleString(byte*str, uint32_t c_pos)
   Point pos = {0,0};
   String str_dt = "";
   uint32_t SizeFont = -1;
-  int strColor = BLUE;
+  int strColor = BLACK;
 
   while (options <= COUNT_STR_ELEMENT_OPTIONS)
   {
@@ -91,8 +102,11 @@ uint32_t CompileSingleString(byte*str, uint32_t c_pos)
         cur_pos += 3;
         break;
       case Color:
-        strColor += (int)(str[cur_pos+1]) << 8;
-        strColor += (int)(str[cur_pos+2]) ;
+        if (!erase)
+        {
+          strColor += (int)(str[cur_pos+1]) << 8;
+          strColor += (int)(str[cur_pos+2]) ;
+        }
         cur_pos += 3;
         break;
       case Size:
@@ -125,16 +139,22 @@ void ReadString(byte*str, uint32_t lenght)
 {
   uint64_t cur_byte = 0;
 
-  Serial.print("3_Start reading: ");
-
+  Serial.println("3_Start reading: ");
   while (cur_byte != lenght)
   {
     switch (str[cur_byte])
     {
       case StringElement:
-        cur_byte = CompileSingleString(str, cur_byte+1);
         cur_byte++;
+        cur_byte = CompileSingleString(str, cur_byte);
+        cur_byte++;
+
+        // tft.setTextSize(5);
+        // tft.setCursor(128,128);
+        // tft.print((int)cur_byte);
+        // tft.print((int)lenght);
         
+        Serial.println("String succeful draw");
         delay (75);
         break;
     }
